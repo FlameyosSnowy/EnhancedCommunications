ab34dd2e1a34a922390b07d236d03bc58f2db686
FlameyosSnowy <eyadamrfawzy7@duck.com>
2024-10-17 16:45:32 +0300

    Fix an issue with the priority system

--- src/main/java/me/flame/communication/modifier/ChatFormatMessageModifier.java
+++ src/main/java/me/flame/communication/modifier/ChatFormatMessageModifier.java
@@ -3,16 +3,14 @@ package me.flame.communication.modifier;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.flame.communication.EnhancedCommunication;
 import me.flame.communication.data.RawDataRegistry;
-import me.flame.communication.providers.ChatProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventPriority;
 import org.jetbrains.annotations.NotNull;
 
 public class ChatFormatMessageModifier implements MessageModifier {
     @Override
-    public EventPriority getPriority() {
-        return EventPriority.HIGHEST;
+    public ModifierPriority getPriority() {
+        return ModifierPriority.HIGHEST;
     }
 
     @Override
--- src/main/java/me/flame/communication/modifier/MentionInsertionMessageModifier.java
+++ src/main/java/me/flame/communication/modifier/MentionInsertionMessageModifier.java
@@ -3,15 +3,13 @@ package me.flame.communication.modifier;
 import me.flame.communication.EnhancedCommunication;
 import me.flame.communication.data.GroupedDataRegistry;
 import me.flame.communication.data.RawDataRegistry;
-import me.flame.communication.managers.ActionsManager;
 import me.flame.communication.managers.MentionsManager;
-import org.bukkit.event.EventPriority;
 import org.jetbrains.annotations.NotNull;
 
 public class MentionInsertionMessageModifier implements MessageModifier {
     @Override
-    public EventPriority getPriority() {
-        return EventPriority.NORMAL;
+    public ModifierPriority getPriority() {
+        return ModifierPriority.NORMAL;
     }
 
     @Override
