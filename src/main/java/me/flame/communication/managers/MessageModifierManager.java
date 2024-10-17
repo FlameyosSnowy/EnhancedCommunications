9ea0706c0ebaf158022a911470f9bd0923df3ea0
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2024-10-17 16:14:21 +0300

    Use improved priority system

--- src/main/java/me/flame/communication/managers/MessageModifierManager.java
+++ src/main/java/me/flame/communication/managers/MessageModifierManager.java
@@ -1,17 +1,13 @@
 package me.flame.communication.managers;
 
-import me.flame.communication.EnhancedCommunication;
 import me.flame.communication.data.RawDataRegistry;
-import me.flame.communication.modifier.ChatFormatMessageModifier;
-import me.flame.communication.modifier.MentionInsertionMessageModifier;
 import me.flame.communication.modifier.MessageModifier;
 import me.flame.communication.utils.Reloadable;
 
 import java.util.Comparator;
-import java.util.PriorityQueue;
 
 public interface MessageModifierManager extends Reloadable {
-    Comparator<MessageModifier> MESSAGE_MODIFIER_COMPARATOR = Comparator.comparing(MessageModifier::getPriority).reversed();
+    Comparator<MessageModifier> MESSAGE_MODIFIER_COMPARATOR = Comparator.comparing(MessageModifier::getPriority);
 
     void addModifier(MessageModifier modifier);
 
