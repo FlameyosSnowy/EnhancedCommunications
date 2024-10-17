de9866aeac73ec832550b90b6184e5361390d987
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2024-10-17 16:15:30 +0300

    Use improved priority system

--- src/main/java/me/flame/communication/modifier/MessageModifier.java
+++ src/main/java/me/flame/communication/modifier/MessageModifier.java
@@ -1,10 +1,9 @@
 package me.flame.communication.modifier;
 
 import me.flame.communication.data.RawDataRegistry;
-import org.bukkit.event.EventPriority;
 
 public interface MessageModifier {
-    EventPriority getPriority();
+    ModifierPriority getPriority();
 
     String modify(RawDataRegistry dataRegistry);
 }
